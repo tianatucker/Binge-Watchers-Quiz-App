fe800583967e53b167299765614d19a40c7fa464
Tiana Tucker <tianatucker@gmail.com>
2017-01-21 01:37:36 -0500

    Minor style changes and  add quiz content copy

---
+++ js/app.js
@@ -0,0 +1,19 @@
+//Fizz Buzz
+
+$(document).ready(function(){
+
+function newQuestion(){
+	$('form').val()
+};
+$('.next-question').click(function(){
+	newQuestion();
+})
+});
+
+//Check that value is selected
+//Check value for correct response
+//Add tally to score if response is correct
+
+
+//Correct answers
+var answers = ['The-Crown','Luke-Cage','The-Man-in-the-High-Castle', 'The-Underwoods', 'The-Mindy-Project']
