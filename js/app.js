620f2c1ffca3032f35135a876f9ebbfc533e13f2
Tiana Tucker <tianatucker@gmail.com>
2017-01-22 15:42:19 -0500

    Modify no radio selected alert

--- js/app.js
+++ js/app.js
@@ -3,10 +3,11 @@ $(document).ready(function(){
 $('#next-btn').click(function(){
 	var userResponse = $('input[name=radio-btn]:checked', '#current-question').val();
 	console.log(userResponse);
-if (userResponse.length === undefined) {
-	alert('Please select an response to continue.');
-} else{
-	alert('its good');
+if (typeof $('input[name=radio-btn]:checked').val() != undefined){
+alert('Select one');
+} else {
+	userAnswers.push('userResponse');
+	console.log(userAnswers);
 }
 })
 });
@@ -15,9 +16,11 @@ if (userResponse.length === undefined) {
 //Check value for correct response
 //Add tally to score if response is correct
 
+//User Responses
+var userAnswers = []
 
 //Correct answers
-var answers = ['The-Crown','Luke-Cage','The-Man-in-the-High-Castle', 'The-Underwoods', 'The-Mindy-Project']
+var correctAnswers = ['The-Crown','Luke-Cage','The-Man-in-the-High-Castle', 'The-Underwoods', 'The-Mindy-Project']
 
 //All Questions
 var questions = ['Which show was the most expensive to produce in television history?','Which show chashed Netflix upon being release during the fall of 2016?','Which show is based on a fictional history novel about the United States losing World War II?','What is the surname of the main characters in the House of Cards?','Which series was canceled by a major tradidtional network after season three then picked up by Hulu?']
